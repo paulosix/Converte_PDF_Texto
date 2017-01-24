d5e49a2054534928599fb6bc718d696c0dbf6445
paulosix <paulosix@hotmail.com>
2017-01-24 14:14:32 -0300

    Retirada de comentarios inuteis

--- Converte_PDF_Texto/Form1.cs
+++ Converte_PDF_Texto/Form1.cs
@@ -44,26 +44,6 @@ namespace Converte_PDF_Texto
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
                 txtCaminoNomePDF.Text = folderBrowserDialog1.SelectedPath;
             
-            //define as propriedades do controle 
-            //OpenFileDialog
-            //this.ofd1.Multiselect = false;
-            //this.ofd1.Title = "Selecionar PDF";
-            //ofd1.InitialDirectory = @"C:\dados";
-            ////filtra para exibir somente arquivos de imagens
-            //ofd1.Filter = "Files (*.PDF)|*.PDF|" + "All files (*.*)|*.*";
-            //ofd1.CheckFileExists = true;
-            //ofd1.CheckPathExists = true;
-            //ofd1.FilterIndex = 2;
-            //ofd1.RestoreDirectory = true;
-            //ofd1.ReadOnlyChecked = true;
-            //ofd1.ShowReadOnly = false;
-            
-            //DialogResult dr = this.ofd1.ShowDialog();
-
-            //if (dr == System.Windows.Forms.DialogResult.OK)
-            //{
-            //    txtCaminoNomePDF.Text = ofd1.FileName;
-            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
